9b68e7ad014522d3280f2869314dc7d7374a923b
Ricardo Vasquez <rvzxjr@gmail.com>
2015-07-21 17:13:53 -0400

    Add sample e2e test

---
+++ spec/e2e/sample.js
@@ -0,0 +1,17 @@
+describe('angularjs homepage todo list', () => {
+
+    it('should add a todo', () => {
+
+        browser.get('https://angularjs.org');
+        element(by.model('todoList.todoText')).sendKeys('write first protractor test');
+        element(by.css('[value="add"]')).click();
+
+        var todoList = element.all(by.repeater('todo in todoList.todos'));
+        expect(todoList.count()).toEqual(3);
+        expect(todoList.get(2).getText()).toEqual('write first protractor test');
+
+        todoList.get(2).element(by.css('input')).click();
+        var completedAmount = element.all(by.css('.done-true'));
+        expect(completedAmount.count()).toEqual(2);
+    });
+});
