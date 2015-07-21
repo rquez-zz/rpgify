369f55182191cb27b02452a6011802d58c7e6348
Ricardo Vasquez <rvzxjr@gmail.com>
2015-07-21 17:14:10 -0400

    Add sample unit api test

---
+++ spec/unit-api/login.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import sinon from 'sinon';
+
+describe("Login Unit Test", () => {
+    it("sample test", () => {
+        expect(5).to.equal(5);
+    });
+});
