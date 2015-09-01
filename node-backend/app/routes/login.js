898b438c136c699ec33250225af1edd919237d5d
Ricardo Vasquez <rvzxjr@gmail.com>
2015-09-01 22:24:23 +0000

    Add blipp module to display routes on npm start

---
+++ node-backend/app/routes/login.js
@@ -0,0 +1,12 @@
+//TODO: Add loginHandler
+import loginHandler from '../handlers/login';
+
+export default [
+    {
+        method: 'POST',
+        path: '/login',
+        config: {
+            handler: loginHandler.login
+        }
+    }
+];
--- node-backend/server.js
+++ node-backend/server.js
@@ -1,10 +1,26 @@
 import Hapi from 'hapi';
 import config from './app/config/rpgify';
 
+import blipp from 'blipp';
+
 var server = new Hapi.Server();
 
 server.connection(config.connection);
 
+server.register([{
+    register: blipp,
+    options: {
+        showAuth: true
+    }
+}], (err) => {
+    if (err) {
+        console.log('Error registering hapi plugins');
+        throw err;
+    }
+});
+
 server.start(() => {
     console.log('Server running at: ' + config.connection.port);
 });
+
+export default server;
