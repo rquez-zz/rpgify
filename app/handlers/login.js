9b9b83df11897fb9b0e62de64fdccb612cccdb65
Ricardo Vasquez <rvzxjr@gmail.com>
2016-01-11 21:46:04 -0500

    Add login handler and initial login/jwt testing.
    So far the username is in a token that is being serialized into the jwt when the user logs
    in. There's no data model set up yet, but the jwt does successfully
    decode into the original token.

--- app/handlers/login.js
+++ app/handlers/login.js
@@ -1,4 +1,12 @@
-// TODO: Add login handler
+import jwt from '../helpers/jwt.js';
+
 export default {
-    login: () => {}
+    login: (req, reply) => {
+
+        var token = {
+            username: req.payload.username
+        };
+
+        reply(jwt.sign(token));
+    }
 };
--- spec/int/login.js
+++ spec/int/login.js
@@ -1,7 +1,57 @@
 import { expect } from 'chai';
+import server from './../../server';
+import jsonwebtoken from 'jsonwebtoken';
+import config from './../../app/config/rpgify';
+import fs from 'fs';
 
-describe("Login Integration Test", () => {
-    it("sample test", () => {
-        expect(5).to.equal(5);
+var login = {
+    username: 'test'
+};
+
+var statusCode;
+var key = fs.readFileSync(config.keyfile);
+
+describe("When user logs in", () => {
+
+    var jwt;
+
+    before(done => {
+        server.inject({
+            url:'/login',
+            method:'POST',
+            payload: login,
+            headers: {
+                'Content-Type':'application/json'
+            }
+        }, response => {
+            statusCode = response.statusCode;
+            jwt = response.payload;
+            done();
+        });
     });
+
+    it("should return a 200 status code", () => {
+        expect(statusCode).to.equal(200);
+    });
+
+    it("should return a jwt", () => {
+        expect(jwt).to.not.be.empty;
+    });
+
+    describe("When jwt is decoded", () => {
+
+        var userToken;
+
+        before(done => {
+            jsonwebtoken.verify(jwt, key, (err, decoded) => {
+                userToken = decoded;
+                done();
+            });
+        });
+
+        it("should have username inside", () => {
+            expect(userToken.username).to.equal(login.username);
+        });
+    });
+
 });
