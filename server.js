5a7da83e4866237ca7a1ea6525255bbecb57ec87
Ricardo Vasquez <rvzxjr@gmail.com>
2016-01-11 19:03:20 -0500

    Register jwt plugins and authentication in server.js

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 import Hapi from 'hapi';
 import config from './app/config/rpgify';
+import jwt from './app/helpers/jwt';
 
+import fs from 'fs';
 import blipp from 'blipp';
 import router from 'hapi-router';
+import hapiAuthJwt from 'hapi-auth-jwt';
 
 var server = new Hapi.Server();
+var key = fs.readFileSync(config.keyfile);
 
 server.connection(config.connection);
 
+// Register plugins, routes, and jwt authentication
 server.register([{
     register: blipp,
     options: {
@@ -18,11 +23,18 @@ server.register([{
     options: {
         routes: './app/routes/*.js'
     }
+}, {
+    register: hapiAuthJwt,
 }], (err) => {
     if (err) {
         console.log('Error registering hapi plugins');
         throw err;
     }
+
+    server.auth.strategy('token', 'jwt', 'required', {
+        validateFunc: jwt.validateToken,
+        key: key
+    });
 });
 
 server.start(() => {
