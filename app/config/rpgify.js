8e36ef186b510d9606b3cb8f71a6359a49937447
Ricardo Vasquez <rvzxjr@gmail.com>
2016-01-11 19:24:49 -0500

    Add more config for jwt and key file

--- app/config/rpgify.js
+++ app/config/rpgify.js
@@ -1,5 +1,11 @@
+var env = process.env;
+
 export default {
     connection: {
-        port: 3000
+        port: env.PORT || 3000
+    },
+    keyfile: env.PRIVATEKEY || 'privateKey',
+    jwtOpts: {
+        issuer: 'rpgify'
     }
 };
