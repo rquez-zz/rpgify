e5ba00564edd7ea7e6c6e1e4c5300162ba2127f4
Ricardo Vasquez <rvzxjr@gmail.com>
2016-01-18 00:27:44 -0500

    Add integration test for deleting user

--- spec/int/rpgify.js
+++ spec/int/rpgify.js
@@ -48,7 +48,7 @@ describe('RPGify Integration Test', () => {
             var login = {
                 username: 'test',
                 password: 'password'
-            }, jwt;
+            }, jwt, token;
 
             before(done => {
                 server.inject({
@@ -63,6 +63,7 @@ describe('RPGify Integration Test', () => {
                 });
             });
 
+
             it("should return a 200 status code", () => {
                 expect(statusCode).to.equal(200);
             });
@@ -73,17 +74,42 @@ describe('RPGify Integration Test', () => {
 
             describe("When jwt is decoded", () => {
 
-                var userToken;
-
                 before(done => {
                     jsonwebtoken.verify(jwt, key, (err, decoded) => {
-                        userToken = decoded;
+                        token = decoded;
                         done();
                     });
                 });
 
                 it("should have username inside", () => {
-                    expect(userToken.username).to.equal(login.username);
+                    expect(token.username).to.equal(login.username);
+                });
+            });
+
+            describe('When a user is deleted', () => {
+                before(done => {
+                    server.inject({
+                        url:'/user',
+                        method:'DELETE',
+                        headers: {
+                            'Content-Type':'application/json',
+                            'Authorization':'Bearer ' + jwt
+                        }
+                    }, response => {
+                        statusCode = response.statusCode;
+                        done();
+                    });
+                });
+
+                it('should return a 204 status code', () => {
+                    expect(statusCode).to.equal(204);
+                });
+
+                it('should have user deleted from db', (done) => {
+                    User.where({ _id: token.userid }).count((err, count) => {
+                        expect(count).to.equal(0);
+                        done();
+                    });
                 });
             });
         });
