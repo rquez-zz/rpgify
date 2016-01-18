a435fde11c8b7acff91bf06d8ce4ca56b24b548d
Ricardo Vasquez <rvzxjr@gmail.com>
2016-01-17 22:38:09 -0500

    Implement create user handler with password hashing

--- app/handlers/user.js
+++ app/handlers/user.js
@@ -1,6 +1,30 @@
-// TODO: Implement userhandler
+import User from '../models/schema.js';
+import bcrypt from 'bcrypt';
+import config from '../config/rpgify';
+
+var SALT_WORK_FACTOR = 10;
+
 export default {
-    createUser: () => {},
+    createUser: (req, reply) => {
+
+        // Hash password and insert user
+        bcrypt.genSalt(config.bcrypt.workFactor, (err, salt) => {
+            if (err) throw err;
+            bcrypt.hash(req.payload.password, salt, (err, hash) => {
+                if (err) throw err;
+
+                User.create({
+                    username: req.payload.username,
+                    password: hash,
+                    firstname: req.payload.firstname,
+                    lastname: req.payload.lastname
+                }, (err, user) => {
+                    if (err) throw err;
+                    return reply().code(204);
+                });
+            });
+        });
+    },
     updateUser: () => {},
     getUser: () => {},
     deleteUser: () => {}
