bf06e847a8630e311b9f2083dfc9cf15d176ee72
Ricardo Vasquez <rvzxjr@gmail.com>
2016-01-17 22:38:52 -0500

    Add db user lookup and bcrypt password compare to login

--- app/handlers/login.js
+++ app/handlers/login.js
@@ -1,12 +1,27 @@
 import jwt from '../helpers/jwt.js';
+import User from '../models/schema.js';
+import boom from 'boom';
+import bcrypt from 'bcrypt';
 
 export default {
     login: (req, reply) => {
 
-        var token = {
-            username: req.payload.username
-        };
+        User.findOne({ username: req.payload.username }, (err, user) => {
+            if (err) throw err;
 
-        reply(jwt.sign(token));
+            var token = {
+                username: req.payload.username,
+                userid: user._id
+            };
+
+            bcrypt.compare(req.payload.password, user.password, (err, isMatch) => {
+                if (err) throw err;
+                if (isMatch) {
+                    reply(jwt.sign(token));
+                } else {
+                    reply(boom.unauthorized('Invalid password'));
+                }
+            });
+        });
     }
 };
