7bcee59b98e5fe90fcc8b13b490d909274603786
Ricardo Vasquez <rvzxjr@gmail.com>
2016-01-21 22:28:30 -0500

    Add integration test for updating user

--- spec/int/rpgify.js
+++ spec/int/rpgify.js
@@ -32,8 +32,8 @@ describe('RPGify Integration Test', () => {
             });
         });
 
-        it("should return a 204 status code", () => {
-            expect(statusCode).to.equal(204);
+        it("should return a 201 status code", () => {
+            expect(statusCode).to.equal(201);
         });
 
         it('should populate database with a user', (done) => {
@@ -86,7 +86,68 @@ describe('RPGify Integration Test', () => {
                 });
             });
 
-            describe('When a user is deleted', () => {
+            describe('When a user is updated successfully', () => {
+
+                var patch = {
+                    name: 'newName',
+                    email: 'new@gmail.com'
+                };
+
+                before(done => {
+                    server.inject({
+                        url:'/user',
+                        method:'PATCH',
+                        payload: patch,
+                        headers: {
+                            'Content-Type':'application/json',
+                            'Authorization':'Bearer ' + jwt
+                        }
+                    }, response => {
+                        statusCode = response.statusCode;
+                        done();
+                    });
+                });
+
+                it("should return a 204 status code", () => {
+                    expect(statusCode).to.equal(204);
+                });
+
+                it("should update user in database", done => {
+                    User.find({ userid: token.userid }, (err, foundUser) => {
+                        expect(foundUser.name === patch.name);
+                        expect(foundUser.email === patch.email);
+                        done();
+                    });
+                });
+            });
+
+            describe('When a user attempts invalid patch', () => {
+
+                var patch = {
+                    firstname: 'newName'
+                };
+
+                before(done => {
+                    server.inject({
+                        url:'/user',
+                        method:'PATCH',
+                        payload: patch,
+                        headers: {
+                            'Content-Type':'application/json',
+                            'Authorization':'Bearer ' + jwt
+                        }
+                    }, response => {
+                        statusCode = response.statusCode;
+                        done();
+                    });
+                });
+
+                it("should return a 422 status code", () => {
+                    expect(statusCode).to.equal(422);
+                });
+            });
+
+            describe('When a user is deleted successfully', () => {
                 before(done => {
                     server.inject({
                         url:'/user',
