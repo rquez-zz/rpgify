a8374bd2db260e77214655e29a3affecb8f0fa65
Ricardo Vasquez <rvzxjr@gmail.com>
2016-03-15 23:47:45 +0000

    Update user lastLogin on login

--- app/handlers/login.js
+++ app/handlers/login.js
@@ -17,9 +17,13 @@ export default {
                 userid: user.userid
             };
 
-            if (user.isValidPassword(req.payload.password))
-                return reply(jwt.sign(token));
-            else
+            if (user.isValidPassword(req.payload.password)) {
+                user.update( { lastLogin: Date.now() }, (err, res) => {
+                    if (err)
+                        return reply(Boom.badImplementation('Error updating user from db', err));
+                    return reply(jwt.sign(token));
+                });
+            } else
                 return reply(Boom.unauthorized('Invalid password'));
         });
     }
