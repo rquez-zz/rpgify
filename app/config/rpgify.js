a31131ef1953ffa0ac550bb70b62da76fd926cbe
Ricardo Vasquez <rvzxjr@gmail.com>
2016-03-18 01:32:46 +0000

    Register hapi-auth-google and setup environment variables

--- app/config/rpgify.js
+++ app/config/rpgify.js
@@ -2,7 +2,9 @@ var env = process.env;
 
 export default {
     connection: { port: env.PORT || 3000 },
+    baseUrl: env.BASE_URL || 'http://localhost:3000',
     key: { path: env.PRIVATEKEY || 'privateKey' },
+    apiKey: { path: env.APIKEY || 'googleApiKey.json'},
     jwt: { issuer: 'rpgify' },
     mongodb: { url:  'mongodb://localhost/rpgify' },
     bcrypt: { workFactor: env.WORK_FACTOR || 10 },
@@ -11,5 +13,9 @@ export default {
     },
     getable: {
         user: 'username name email skills userExp signup lastLogin -_id'
+    },
+    googleOpts: {
+        REDIRECT_URL: '/auth-callback',
+        scope: 'https://www.googleapis.com/auth/plus.profile.emails.read'
     }
 };
--- server.js
+++ server.js
@@ -2,19 +2,34 @@ import Hapi from 'hapi';
 import config from './app/config/rpgify';
 import jwt from './app/helpers/jwt';
 import db from './app/helpers/db';
+import auth from './app/handlers/auth';
 
 import fs from 'fs';
 import blipp from 'blipp';
 import router from 'hapi-router';
 import hapiAuthJwt from 'hapi-auth-jwt';
+import hapiAuthGoogle from 'hapi-auth-google';
 
 var server = new Hapi.Server();
+
 var key = fs.readFileSync(config.key.path);
+
+var apiKey = fs.readFileSync(config.apiKey.path);
+var apiKeyJson = JSON.parse(apiKey);
+
+process.env.GOOGLE_CLIENT_ID = apiKeyJson.web.client_id;
+process.env.GOOGLE_CLIENT_SECRET = apiKeyJson.web.client_secret;
+process.env.BASE_URL = config.baseUrl;
+process.env.PORT = config.connection.port;
 
 server.connection(config.connection);
 
-// Register plugins, routes, and jwt authentication
+config.googleOpts.handler = auth.authHandler;
+
 server.register([{
+    register: hapiAuthGoogle,
+    options: config.googleOpts
+},{
     register: blipp,
     options: {
         showAuth: true
@@ -31,7 +46,6 @@ server.register([{
         console.log('Error registering hapi plugins');
         throw err;
     }
-
     server.auth.strategy('token', 'jwt', 'required', {
         validateFunc: jwt.validateToken,
         key: key
